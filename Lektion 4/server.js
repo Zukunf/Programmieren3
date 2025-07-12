746fc59dc50250e9e8673197eb2f5c1f448162cb
zukunftplan zukunftplan <Zukunf>
2025-07-12 15:48:05 +0200

    chat app

--- Lektion 4/server.js
+++ Lektion 4/server.js
@@ -1,33 +1,30 @@
 var express = require('express');
+var fs = require('fs');
+
+
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
 var messages = [];
 
 app.use(express.static("."));
+
 app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
 
-res.redirect('index.html');
-
+io.on('connection', function (socket) {
+    console.log("connected")
+    for (var i in messages) {
+        io.sockets.emit("display message", messages[i]);
+    }
+    socket.on("send message", function (data) {
+        console.log("recieving message")
+        messages.push(data);
+        fs.writeFileSync("messages.json", JSON.stringify(messages));
+        io.sockets.emit("display message", data);
+    });
 });
 
 server.listen(3000);
-
-io.on('connection', function (socket) {
-
-    for(var i in messages) {
-    
-    io.sockets.emit("display message",
-    messages[i]);
-    
-    }
-    
-    socket.on("send message", function (data) {
-    
-    messages.push(data);
-    
-    io.sockets.emit("display message",
-    data);
-    
-    });
-});
